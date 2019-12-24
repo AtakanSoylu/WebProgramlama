217aa07d0b909f6412f1923d1c7f6846f415d936
AtakanSoylu <59143350+AtakanSoylu@users.noreply.github.com>
2019-12-24 18:03:53 +0300

    Add files via upload

---
+++ Data/KullaniciContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+namespace WebApplication3.Data
+{
+    public class KullaniciContext : DbContext
+    {
+        public KullaniciContext (DbContextOptions<KullaniciContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebApplication3.Models.Urun> Urun { get; set; }
+        public DbSet<WebApplication3.Models.Ulke> Ulke { get; set; }
+        public DbSet<WebApplication3.Models.Kullanici> Kullanici { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Urun>().ToTable("Urun");
+            modelBuilder.Entity<Ulke>().ToTable("Ulke");
+            modelBuilder.Entity<Kullanici>().ToTable("Kullanici");
+        }
+    }
+}
---
+++ Models/Ulke.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication3.Models
+{
+    public class Ulke
+    {
+        public string Id { get; set; }
+        public string Ulkeismi { get; set; }
+        public string Kita { get; set; }
+        public string Dil { get; set; }
+        public string UrunId { get; set; }
+
+        [ForeignKey("UrunId")]
+        public virtual Urun Urun { get; set; }
+        public string KullaniciId { get; set; }
+        [ForeignKey("KullaniciId")]
+        public virtual Kullanici Kullanici { get; set; }
+        public virtual ICollection<Urun> Urun1 { get; set; }
+        public virtual ICollection<Kullanici> Kullanici1 { get; set; }
+    }
+
+
+
+}
